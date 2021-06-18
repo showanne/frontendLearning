e2032e9b919c5dab3a105ac26ef52752ab289d95
showanne <showanne.e@gmail.com>
2021-06-18 22:29:22 +0800

    Vue 0608 補部分上課筆記

--- FrontDevelop-js.jQuery/1100608 vue/club/src/router/index.js
+++ FrontDevelop-js.jQuery/1100608 vue/club/src/router/index.js
@@ -29,10 +29,22 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: 'guitar-learning' */ '../views/GuitarLearning.vue')
   },
+  // {
+  //   path: '/settings',        當網頁路徑 為 '/    ' 時
+  //   name: 'Settings',         網頁名稱叫 '     '
+  //   component: () => import(/* webpackChunkName: "settings" */ '../views/Settings.vue'),                lazyload 當網頁展入時，還沒點到此頁，不會先載入
+  //   meta: {
+  //     title: '設定'   網頁標籤名稱
+  //   }
+  // }
   {
-    path: '*',
+    // 一定要放在最後面  -- 找不到頁面
+    // 網頁路徑為沒有設定的網頁(所有能配對的都配對過，從上面抓不到)會顯示的頁面
+    // 用意：不會看到空頁白白一片
+    path: '*',   
     name: 'NotFound',
     component: NotFound
+    // 最上面記得要引用 import NotFound from '../views/NotFound.vue'
   }
   // ,
   // {
--- FrontDevelop-js.jQuery/1100608 vue/club/vue.config.js
+++ FrontDevelop-js.jQuery/1100608 vue/club/vue.config.js
@@ -1,3 +1,6 @@
+// commonJS 語法
 module.exports = {
   publicPath: './'
+  // 這樣打包時才不會出錯
+  // 打包 npm run build
 }
